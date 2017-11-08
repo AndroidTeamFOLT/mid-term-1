1023b6e7e3fc3e782ba956f8a354a292fb30131b
SHUXIN <tangshx@mail2.sysu.edu.cn>
2017-11-08 23:30:51 +0800

    test2

--- app/src/main/java/com/example/victory/mid_term/MainActivity.java
+++ app/src/main/java/com/example/victory/mid_term/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//jgfgfd
+        setContentView(R.layout.activity_main);//jgfgfddfffdgdh
     }
 }
